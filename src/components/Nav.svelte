0ca34e52cdcc1ae0943bc404d29aa874b56dc4dd
Bryan Lee <bryanmylee@gmail.com>
2020-12-17 19:49:15 +0000

    fix: use mask-image to provide color for styled nav bar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -5,20 +5,28 @@
   export let segment: string;
 </script>
 
-<div
-    class="flex justify-center bg-repeat text-accent"
-    style={`
-      background-image: url("${navBarBg}");
-      background-size: 12px 4px;
-    `}
-    >
-  <div class="flex items-stretch justify-between w-full max-w-4xl">
-    <div class="flex items-center bg-accent">
-      <span class="p-2 text-base font-bold">
-        Bryan Lee (bryanmylee)
-      </span>
+<div class="relative">
+  <div
+      class="absolute inset-0 bg-accent"
+      style={`
+        z-index: -1;
+        -webkit-mask-image: url("${navBarBg}");
+        mask-image: url("${navBarBg}");
+        -webkit-mask-size: 12px 4px;
+        mask-size: 12px 4px;
+        -webkit-mask-repeat: repeat;
+        mask-repeat: repeat;
+      `}
+      />
+  <nav class="flex justify-center">
+    <div class="flex items-stretch justify-between w-full max-w-4xl">
+      <div class="flex items-center bg-accent">
+        <span class="p-2 text-base font-bold">
+          Bryan Lee (bryanmylee)
+        </span>
+      </div>
+      <NavContactLinks class="flex p-2 text-base bg-accent" size={8} />
     </div>
-    <NavContactLinks class="flex p-2 text-base bg-accent" size={8} />
-  </div>
+  </nav>
 </div>
 
