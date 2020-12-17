eafbbeb0c37578b931821de2cdcc0d5583f15348
Bryan Lee <bryanmylee@gmail.com>
2020-12-17 03:23:58 +0000

    build: use gh-pages branch

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,14 +2,6 @@ const ghpages = require('gh-pages');
 
 ghpages.publish(
   '__sapper__/export',
-  {
-    branch: 'dist',
-    repo: 'git@github.com:bryanmylee/website.git',
-    user: {
-      name: 'bryanmylee',
-      email: 'bryanmylee@gmail.com',
-    },
-  },
   () => {
     console.log('Deploy complete!');
   }
