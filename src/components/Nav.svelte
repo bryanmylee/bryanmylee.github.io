bbf933055c7082664ced71b8acb0d04e64a08c46
Bryan Lee <bryanmylee@gmail.com>
2020-12-17 20:03:27 +0000

    style: adjust margins

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -5,16 +5,16 @@
   export let segment: string;
 </script>
 
-<div class="relative">
+<div class="relative mt-4">
   <NavBg class="absolute inset-0 bg-accent -z-1"/>
   <nav class="flex justify-center">
     <div class="flex items-stretch justify-between w-full max-w-4xl">
       <div class="flex items-center bg-accent">
-        <span class="p-2 text-base font-bold">
+        <span class="px-3 py-1 text-base font-bold">
           Bryan Lee (bryanmylee)
         </span>
       </div>
-      <NavContactLinks class="flex p-2 text-base bg-accent" size={8} />
+      <NavContactLinks class="flex py-1 pr-3 text-base bg-accent" size={8} />
     </div>
   </nav>
 </div>
