21e418d2494a6a1e5e4c19e57a657069d5906959
Bryan Lee <bryanmylee@gmail.com>
2020-12-19 01:08:02 +0800

    feat: remove subnav from layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-  export let segment: string;
-
   import Nav from '@my/components/Nav';
-  import SubNav from '@my/components/SubNav';
+
+  export let segment: string;
 </script>
 
 <Nav {segment}/>
-<SubNav {segment}/>
 <main class="max-w-4xl mx-auto my-0">
   <slot/>
 </main>
