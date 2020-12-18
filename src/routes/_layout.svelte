796f0e7b0f8156bec6767a684b67535b0957cf35
Bryan Lee <bryanmylee@gmail.com>
2020-12-19 00:44:11 +0800

    style: export default nav from folder

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
   export let segment: string;
 
-  import Nav from '@my/components/Nav.svelte';
+  import Nav from '@my/components/Nav';
 </script>
 
 <Nav {segment}/>
 <main class="max-w-4xl mx-auto my-0">
   <slot/>
 </main>
-
