1747898b89165f629531fe2d1c17062efd172341
Bryan Lee <bryanmylee@gmail.com>
2020-12-19 01:29:00 +0800

    feat: add subnav to layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
   import Nav from '@my/components/Nav';
+  import SubNav from '@my/components/SubNav';
 
   export let segment: string;
 </script>
 
 <Nav {segment}/>
+<SubNav class="flex max-w-4xl mx-auto"/>
 <main class="max-w-4xl mx-auto my-0">
   <slot/>
 </main>
+
