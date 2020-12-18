21f111c14a22694037350c39be80dcd9c4ed4ebb
Bryan Lee <bryanmylee@gmail.com>
2020-12-18 01:35:59 +0000

    style: add transition for nav bar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -5,7 +5,7 @@
   export let segment: string;
 </script>
 
-<div class="relative mt-4">
+<div class="relative lg:mt-5 transition-all">
   <NavBg class="absolute inset-0 bg-accent -z-1"/>
   <nav class="flex justify-center">
     <div class="flex items-stretch justify-between w-full max-w-4xl">
@@ -14,7 +14,7 @@
           Bryan Lee (bryanmylee)
         </span>
       </div>
-      <NavContactLinks class="flex py-1 pr-3 text-base bg-accent" size={8} />
+      <NavContactLinks class="flex py-1 pr-3 text-base bg-accent" size={8}/>
     </div>
   </nav>
 </div>
