6c062f4f5a0cdcd2f1e74ba3f1af65b19fe76446
Bryan Lee <bryanmylee@gmail.com>
2020-12-19 00:52:55 +0800

    feat: implement subnav

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,9 +2,11 @@
   export let segment: string;
 
   import Nav from '@my/components/Nav';
+  import SubNav from '@my/components/SubNav';
 </script>
 
 <Nav {segment}/>
+<SubNav {segment}/>
 <main class="max-w-4xl mx-auto my-0">
   <slot/>
 </main>
