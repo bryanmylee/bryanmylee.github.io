c3148eb31264d3ccf47b5c394d0ff3514b8691aa
Bryan Lee <bryanmylee@gmail.com>
2020-12-20 01:05:56 +0800

    feat: separate data and view concerns, add directory index

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   import Nav from '@my/components/Nav';
   import SubNav from '@my/components/SubNav';
+  import links from './_links';
 
   export let segment: string;
 </script>
 
 <Nav/>
-<SubNav {segment}/>
+<SubNav {segment} {links} />
 <main class="max-w-4xl mx-auto my-0">
   <slot/>
 </main>
---
+++ src/routes/_links.ts
@@ -0,0 +1,15 @@
+/**
+ * Index of subroutes available.
+ */
+import type Link from '@my/models/Link';
+
+const links: Link[] = [
+  { name: 'blog', href: 'blog' },
+  { name: 'projects', href: 'projects' },
+  { name: 'skills', href: 'skills' },
+  { name: 'experience', href: 'experience' },
+  { name: 'contact', href: 'contact' },
+];
+
+export default links;
+
