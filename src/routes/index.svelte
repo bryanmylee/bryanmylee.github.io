bb6facf40c4c38743297e6aca3cfe9d7f548942d
Bryan Lee <bryanmylee@gmail.com>
2020-12-23 02:42:30 +0800

    feat: add first paragraph

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,7 @@
 </svelte:head>
 
 <script lang="ts">
+  import Card from '@my/components/Card';
   import IntroHeader from '@my/components/IntroHeader';
 
   const introDescriptions = [
@@ -12,5 +13,11 @@
   ];
 </script>
 
-<IntroHeader descriptions={introDescriptions} class="px-3"/>
+<IntroHeader descriptions={introDescriptions} class="px-3 mt-2 text-lg"/>
+
+<div class="mx-3 mt-4 space-y-2">
+  <Card class="p-3">
+    Talk to me about all things tech!
+  </Card>
+</div>
 
