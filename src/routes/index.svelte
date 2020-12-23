84cccfdf8cffaf22bf37ad7bcfae5c8fbb1c7436
Bryan Lee <bryanmylee@gmail.com>
2020-12-23 18:42:37 +0800

    feat: add profile image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,7 @@
 
 <script lang="ts">
   import IntroHeader from '@my/components/IntroHeader';
+  import profilePhoto from '@my/assets/profile.jpg';
 
   const introDescriptions = [
     'computer science student',
@@ -14,23 +15,29 @@
 
 <IntroHeader descriptions={introDescriptions} class="px-3 mt-2 font-mono text-xl"/>
 
-<div class="mx-3 mt-4 space-y-3">
-  <p class="p-3 card leading-8">
-    Talk to me about all things tech! I am a computer geek first and foremost,
-    with a passion for software development. In my free time, I enjoy building
-    tools and writing scripts that solve real-world problems.
-  </p>
-  <p class="p-3 card leading-8">
-    I am currently pursuing a computer science degree from Singapore Management
-    University, but I also enjoy learning outside of class and keeping up with
-    the industry. I am especially interested in software architecture and design
-    patterns, and I always strive to write beautiful code.
-  </p>
-  <p class="p-3 card leading-8">
-    To learn more about me, check out my <a href="/projects">projects</a> and
-    <a href="/experience">experience</a>. You can also read my
-    <a href="/blog">blog</a> where I share interesting lessons that I have
-    learnt.
-  </p>
+<div class="
+    flex flex-col mt-3 space-y-3
+    md:flex-row md:mx-3 md:space-x-3 md:space-y-0"
+    >
+  <div class="space-y-3 para">
+    <p class="p-3 card leading-8">
+      Talk to me about all things tech! I am a computer geek first and foremost,
+      with a passion for software development. In my free time, I enjoy building
+      tools and writing scripts that solve real-world problems.
+    </p>
+    <p class="p-3 card leading-8">
+      I am currently pursuing a computer science degree from Singapore Management
+      University, but I also enjoy learning outside of class and keeping up with
+      the industry. I am especially interested in software architecture and design
+      patterns, and I always strive to write beautiful code.
+    </p>
+    <p class="p-3 card leading-8">
+      To learn more about me, check out my <a href="/projects">projects</a> and
+      <a href="/experience">experience</a>. You can also read my
+      <a href="/blog">blog</a> where I share interesting lessons that I have
+      learnt.
+    </p>
+  </div>
+  <img src={profilePhoto} alt="profile" class="md:max-h-96">
 </div>
 
