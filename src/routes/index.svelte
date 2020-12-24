41d733bc6c185fc8174752d2e2e6066342a8823d
Bryan Lee <bryanmylee@gmail.com>
2020-12-24 17:43:08 +0800

    feat: add project previews to homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,6 +6,8 @@
   import IntroHeader from '@my/components/IntroHeader';
   import IntroBody from '@my/components/IntroBody';
   import IntroExperience from '@my/components/IntroExperience';
+  import IntroProjects from '@my/components/IntroProjects';
+
   const introDescriptions = [
     'computer science student',
     'vim enthusiast',
@@ -16,4 +18,5 @@
 <IntroHeader descriptions={introDescriptions}/>
 <IntroBody class="mt-3"/>
 <IntroExperience class="mt-10"/>
+<IntroProjects class="mt-10"/>
 
