b9eaa49198d40fdecf9f495376fb056549f4e647
Bryan Lee <bryanmylee@gmail.com>
2020-12-27 04:22:35 +0800

    style: trim to 80ch

--- src/routes/experience/index.svelte
+++ src/routes/experience/index.svelte
@@ -28,18 +28,21 @@
       <div class="space-y-3">
         <div class="card p-3 leading-8 prose">
           I am the <strong>President of the Whitehat Society</strong> &mdash; a
-          cybersecurity interest group &mdash; in 2021. We believe in open knowledge, and
-          strive to empower future generations of programmers and cybersecurity
-          enthusiasts. In our mission to advocate information security to our
-          school community and beyond, we organize cybersecurity competitions and
-          training workshops.
+          cybersecurity interest group &mdash; in 2021. We believe in open
+          knowledge, and strive to empower future generations of programmers
+          and cybersecurity enthusiasts. In our mission to advocate information
+          security to our school community and beyond, we organize cybersecurity
+          competitions and training workshops.
         </div>
 
         <h3 class="font-bold">Projects</h3>
 
         <ExperienceProject projectTitle="Cyber Hacking Fundamentals">
           <ul>
-            <li>Initiated and conducted a beginner-oriented workshop that teaches the basics of cybersecurity.</li>
+            <li>
+              Initiated and conducted a beginner-oriented workshop that teaches
+              the basics of cybersecurity.
+            </li>
           </ul>
         </ExperienceProject>
 
