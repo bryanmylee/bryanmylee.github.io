28c7b7cc711bbf1a7e800c2fc82458e8df25850d
Bryan Lee <bryanmylee@gmail.com>
2020-12-27 19:07:02 +0800

    style: trim to 80ch

--- src/routes/experience/_data.ts
+++ src/routes/experience/_data.ts
@@ -64,13 +64,16 @@ export default {
           projectName: 'Bonding Day CTF',
           tasks: [
             'Organised a day of orientation and games for Whitehat members.',
-            'Developed and hosted a capture-the-flag (CTF) competition for the event.',
+            `Developed and hosted a capture-the-flag (CTF) competition for the
+             event.`,
           ],
         },
         {
           projectName: 'Whitehacks 2021',
           tasks: [
-            'Executed a Junior College (JC) outreach programme comprising training workshops, a CTF competition, and networking opportunities with industry partners.',
+            `Executed a Junior College (JC) outreach programme comprising
+            training workshops, a CTF competition, and networking opportunities
+            with industry partners.`,
             'Liaised with partners to secure funding and support.',
             'Recruited and managed an organising committee.',
           ],
