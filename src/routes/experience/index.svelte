90cc443078087dd83396b12f37d23149eaa3cd8b
Bryan Lee <bryanmylee@gmail.com>
2020-12-27 17:28:37 +0800

    feat: implement styles for experience projects

--- src/routes/experience/index.svelte
+++ src/routes/experience/index.svelte
@@ -3,7 +3,6 @@
 </svelte:head>
 
 <script lang="ts" context="module">
-  import ExperienceBody from '@my/components/ExperienceBody';
   import whitehatsLogo from '@my/assets/logos/whitehats-logo.png';
   import garudaRoboticsLogo from '@my/assets/logos/garuda-robotics-logo.png';
   import codeGakkoLogo from '@my/assets/logos/code-gakko-logo.png';
@@ -25,28 +24,36 @@
       position: 'Educator',
       logo: codeGakkoLogo,
     },
-  }
+  };
 </script>
 
 <script lang="ts">
+  import ExperienceBody from '@my/components/ExperienceBody';
+  import ExperienceProjects from '@my/components/ExperienceProjects';
   import ExperienceSelector from '@my/components/ExperienceSelector';
+  import type ExperienceProject from '@my/models/ExperienceProject';
 
   let selectedKey = 'whitehats';
   $: selected = experiences[selectedKey];
 
-  const getSummary = async (key: string) => {
-    return (await import(`./_summary/${key}.mdx`)).default;
-  }
+  const getSummary = async (experience: string) => {
+    return (await import(`./_data/${experience}/summary.mdx`)).default;
+  };
+  const getProjects = async (experience: string) => {
+    return (await import(`./_data/${experience}/projects`)).default as ExperienceProject[];
+  };
 </script>
 
 <div class="flex space-x-3">
   <ExperienceSelector bind:selectedKey {experiences}/>
-  {#await getSummary(selectedKey) then Summary}
-    <ExperienceBody companyName={selected.companyName} {Summary}/>
-  {:catch}
-    <p>
-      No summary found.
-    </p>
-  {/await}
+  <div class="space-y-3">
+    <h2 class="mb-3">{selected.companyName}</h2>
+    {#await getSummary(selectedKey) then Summary}
+      <ExperienceBody {Summary}/>
+    {/await}
+    {#await getProjects(selectedKey) then projects}
+      <ExperienceProjects {projects}/>
+    {/await}
+  </div>
 </div>
 
