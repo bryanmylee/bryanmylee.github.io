40005bcd6998a665020130e04bbc273ec0c6a35c
Bryan Lee <bryanmylee@gmail.com>
2020-12-30 19:10:44 +0800

    feat: sticky nav bar to top of page

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -10,10 +10,12 @@
   const quickFade = (node: Element) => fade(node, { duration: 200 });
 </script>
 
-<Nav/>
-<SubNav {segment} links={subdirs}/>
+<nav class="sticky top-0 pb-3 lg:pt-3 transition-all">
+  <Nav/>
+  <SubNav {segment} links={subdirs}/>
+</nav>
 <PageTransitions refresh={segment} transition={quickFade}>
-  <main class="max-w-screen-lg p-3 mx-auto my-0 mt-6">
+  <main class="p-3 mx-auto my-0 mt-3 max-w-screen-lg">
     <slot/>
   </main>
 </PageTransitions>
