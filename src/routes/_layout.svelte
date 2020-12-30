a52b311c3c47c0f17f3253194ae10fd0e466ddf3
Bryan Lee <bryanmylee@gmail.com>
2020-12-30 19:40:56 +0800

    feat: white border below nav on overflow

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -5,17 +5,23 @@
   import SubNav from '@my/components/SubNav';
   import { subdirs } from './index.svelte';
 
-  export let segment: string;
-
   const quickFade = (node: Element) => fade(node, { duration: 200 });
+
+  export let segment: string;
+  let scrollY = 0;
 </script>
 
-<nav class="sticky top-0 z-10 pb-3 lg:pt-3 transition-all bg-base">
+<svelte:window bind:scrollY/>
+<nav class={`
+  fixed top-0 left-0 right-0 z-10 pb-3
+  border-b ${scrollY > 0 ? 'border-white' : 'border-transparent'}
+  lg:pt-3 transition-all bg-base`}
+  >
   <Nav/>
   <SubNav {segment} links={subdirs}/>
 </nav>
 <PageTransitions refresh={segment} transition={quickFade}>
-  <main class="p-3 mx-auto my-0 mt-3 max-w-screen-lg">
+  <main class="p-3 mx-auto my-0 mt-28 max-w-screen-lg">
     <slot/>
   </main>
 </PageTransitions>
