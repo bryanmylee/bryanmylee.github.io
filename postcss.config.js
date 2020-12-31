c714f8ff0afa6691a1de08ed4f829ed0c265e41c
Bryan Lee <bryanmylee@gmail.com>
2021-01-01 02:34:56 +0800

    feat: disable cssnano on dev

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,18 @@
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+
+const prodPlugins = [
+  require('cssnano')({
+    preset: 'default',
+  }),
+];
+
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-nested'),
     require('tailwindcss'),
-    require('cssnano')({
-      preset: 'default',
-    }),
+    ...(dev ? [] : prodPlugins),
   ],
 };
 
