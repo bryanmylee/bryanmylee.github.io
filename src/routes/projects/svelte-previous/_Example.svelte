d5e0adf7954cc5ef2b314fdcddd194998fd98732
Bryan Lee <bryanmylee@gmail.com>
2021-01-01 17:09:34 +0000

    feat: extract typing transition

--- src/routes/projects/svelte-previous/_Example.svelte
+++ src/routes/projects/svelte-previous/_Example.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import { usePrevious } from 'svelte-previous';
+  import { backspaceAndType } from '@my/transitions/backspaceAndType';
   import indexData from '../../_data';
   const items = indexData.headers;
   let index = 0;
   const [currentItem, previousItem] = usePrevious(items[index]);
+  $currentItem = '';
   $: $currentItem = items[index];
   onMount(() => {
     const timer = setInterval(() => {
@@ -12,32 +14,10 @@
     }, 3000);
     return () => clearInterval(timer);
   });
-  function glitch(node: Element, { speed = 50, from = '' }) {
-    const valid
-        = node.childNodes.length === 1
-        && node.childNodes[0].nodeType === Node.TEXT_NODE;
-    if (!valid) {
-      throw new Error(`This transition only works on elements with a single text node child`);
-    }
-    const to = node.textContent;
-    const duration = (from.length + to.length) * speed;
-    const fromRatio = from.length / (from.length + to.length);
-    const toRatio = 1 - fromRatio;
-    return {
-      duration,
-      tick: (t: number) => {
-        if (t <= fromRatio) {
-          node.textContent = from.slice(0, ~~(from.length * (fromRatio - t) / fromRatio));
-        } else {
-          node.textContent = to.slice(0, ~~(to.length * (t - fromRatio) / toRatio));
-        }
-      },
-    };
-  }
 </script>
 
 {#key $currentItem}
-  <div in:glitch={{from: $previousItem}}>
+  <div in:backspaceAndType={{from: $previousItem}}>
     {$currentItem}
   </div>
 {/key}
