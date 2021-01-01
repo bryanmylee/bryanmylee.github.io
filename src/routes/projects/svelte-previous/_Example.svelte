8e0b3fd7a761f95df636e60f665864499b08449a
Bryan Lee <bryanmylee@gmail.com>
2021-01-01 11:27:25 +0800

    feat: use onMount with destroy callback

--- src/routes/projects/svelte-previous/_Example.svelte
+++ src/routes/projects/svelte-previous/_Example.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
-  import { onDestroy } from 'svelte';
+  import { onMount } from 'svelte';
   import { usePrevious } from 'svelte-previous';
   import indexData from '../../_data';
   const items = indexData.headers;
   let index = 0;
   const [currentItem, previousItem] = usePrevious(items[index]);
-  const timer = setInterval(() => {
-    index = (index + 1) % items.length;
-  }, 3000);
   $: $currentItem = items[index];
-  onDestroy(() => clearInterval(timer));
+  onMount(() => {
+    const timer = setInterval(() => {
+      index = (index + 1) % items.length;
+    }, 3000);
+    return () => clearInterval(timer);
+  })
 </script>
 
 {$currentItem} and {$previousItem}
