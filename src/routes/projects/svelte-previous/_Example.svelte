9b9b7cd42f6d2dc589844ebf0409c395cad35404
Bryan Lee <bryanmylee@gmail.com>
2021-01-01 18:22:57 +0000

    feat: use transition event

--- src/routes/projects/svelte-previous/_Example.svelte
+++ src/routes/projects/svelte-previous/_Example.svelte
@@ -5,20 +5,22 @@
   import indexData from '../../_data';
   const items = indexData.headers;
   let index = 0;
-  const [currentItem, previousItem] = usePrevious(items[index]);
-  $currentItem = '';
+  const [currentItem, previousItem] = usePrevious('');
   $: $currentItem = items[index];
-  onMount(() => {
-    const timer = setInterval(() => {
+  const delayedUpdate = () => {
+    setTimeout(() => {
       index = (index + 1) % items.length;
     }, 3000);
-    return () => clearInterval(timer);
-  });
+  };
+  onMount(delayedUpdate);
 </script>
 
 <div class="flex items-center justify-center h-12 font-mono">
   {#key $currentItem}
-    <div in:backspaceAndType={{from: $previousItem}}>
+    <div
+      in:backspaceAndType={{from: $previousItem}}
+      on:introend={delayedUpdate}
+      >
       {$currentItem}
     </div>
   {/key}
