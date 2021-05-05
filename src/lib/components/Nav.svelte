70d6c540911b96f3dc273c222f0612f9582b2982
Bryan Lee <bryanmylee@gmail.com>
2021-05-05 14:17:21 +0800

    feat: sticky nav

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,11 +1,21 @@
-<div class="relative font-mono">
-  <div class="absolute inset-0 bg-yellow -z-1 nav-bg" style={`--bg: url(nav-bg.svg)`} />
-  <div class="flex items-stretch justify-between w-full max-w-screen-md mx-auto">
-    <a href="/" class="flex items-center p-2 font-mono text-base text-xl font-bold bg-yellow">
-      Bryan Lee
-    </a>
+<nav class="sticky top-0">
+  <div class="relative font-mono">
+    <div class="absolute inset-0 bg-yellow -z-1 nav-bg" style={`--bg: url(nav-bg.svg)`} />
+    <div class="flex items-stretch justify-between w-full max-w-screen-md mx-auto">
+      <a href="/" class="flex items-center p-2 font-mono text-base text-xl font-bold bg-yellow">
+        Bryan Lee
+      </a>
+    </div>
   </div>
-</div>
+</nav>
+
+<ul class="flex w-full max-w-screen-md mx-auto mt-6 space-x-4 font-mono underline">
+  <li><a href="/blog">blog</a></li>
+  <li><a href="#projects">projects</a></li>
+  <li><a href="#skills">skills</a></li>
+  <li><a href="#experience">experience</a></li>
+  <li><a href="#contact">contact</a></li>
+</ul>
 
 <style lang="postcss">
   .nav-bg {
