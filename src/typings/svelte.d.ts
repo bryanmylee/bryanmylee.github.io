45ec37104c598ddb19173dc84cc1cafe9a624d84
Bryan Lee <bryanmylee@gmail.com>
2021-05-05 20:29:48 +0800

    fix: extend instead of replace svelte module

--- src/typings/svelte.d.ts
+++ src/typings/svelte.d.ts
@@ -1,3 +1,5 @@
+import * as svelte from 'svelte';
+
 declare module 'svelte' {
   interface Action<T> {
     (node: HTMLElement, params: T): { update?: (newParams: T) => void; destroy?: () => void };
