2e8a2e071585efa00469a4516a150f0fac4f0ced
Bryan Lee <bryanmylee@gmail.com>
2021-05-06 01:33:57 +0800

    feat: skill description on hover

--- src/lib/components/SkillCloud.svelte
+++ src/lib/components/SkillCloud.svelte
@@ -6,32 +6,19 @@
   export let skills: Skill[] = [];
 
   export let cloudScale = 60;
+
+  // prettier-ignore
   const unitDisplacements = [
-    [0, 0],
-    [-2, -1],
-    [-2, 1],
-    [0, 2.24],
-    [2, 1],
-    [2, -1],
-    [0, -2.24],
-    [-4, 0],
-    [-4, 2.24],
-    [4, 0],
-    [-6, -1],
-    [-6, 1],
-    [6, -1],
-    [6, 1]
+    [0, 0], [-2, -1], [-2, 1], [0, 2.24], [2, 1], [2, -1], [0, -2.24],
+    [-4, 0], [-4, 2.24], [4, 0], [-6, -1], [-6, 1], [6, -1], [6, 1]
   ];
   $: displacements = unitDisplacements.map(([x, y]) => [x * cloudScale, y * cloudScale]);
 </script>
 
 <section class="flex items-center justify-center w-full h-[400px]">
-  <div class="relative">
+  <div class="relative transform -translate-x-14 -translate-y-14">
     {#each zip(skills, displacements) as [skill, [x, y]]}
-      <div
-        class="absolute transform -translate-x-1/2 -translate-y-1/2"
-        style="left: {x}px; top: {y}px"
-      >
+      <div class="absolute" style="left: {x}px; top: {y}px">
         <SkillCloudItem {...skill} />
       </div>
     {/each}
--- src/lib/components/SkillCloudItem.svelte
+++ src/lib/components/SkillCloudItem.svelte
@@ -1,13 +1,31 @@
 <script lang="ts">
   import CursorGravity from '$lib/components/CursorGravity.svelte';
+  import { createPopperActions } from 'svelte-popperjs';
 
   export let name = '';
   export let description = '';
   export let imageSrc = '';
 
-  let size = 4;
+  const [ref, content] = createPopperActions();
+  let showPopper = false;
 </script>
 
+{#if showPopper}
+  <div use:content class="fixed z-10 p-4 space-y-2 w-80 bg-base-lighter">
+    <h2 class="font-mono text-xl font-bold text-yellow">{name}</h2>
+    <p class="text-sm leading-6">
+      {description}
+    </p>
+  </div>
+{/if}
+
 <CursorGravity>
-  <img src={imageSrc} alt={name} class="w-28 h-28 min-w-28 min-h-28" />
+  <img
+    src={imageSrc}
+    alt={name}
+    use:ref
+    on:mouseenter={() => (showPopper = true)}
+    on:mouseout={() => (showPopper = false)}
+    class="z-0 w-28 h-28 min-w-28 min-h-28"
+  />
 </CursorGravity>
