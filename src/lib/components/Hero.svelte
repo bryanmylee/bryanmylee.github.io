61583356c8219a5e1382faded77dfd32050fa68e
Bryan Lee <bryanmylee@gmail.com>
2021-05-05 13:36:44 +0800

    style: underline anchor on hover

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -7,6 +7,6 @@
   <h1 class="text-4xl text-yellow font-bold font-mono">Bryan Lee (bryanmylee)</h1>
   <p>I am a computer geek with a passion for software development.</p>
   <p>I build tools and sceipts that solve real-world problems.</p>
-  <a href="#projects" class="block text-2xl text-cyan font-bold font-mono">Check them out here.</a>
+  <a href="#projects" class="block text-2xl text-cyan font-bold font-mono hover:underline">Check them out here.</a>
   <img src="profile.png" alt="profile" class="absolute right-0 bottom-0 w-[300px]" />
 </section>
