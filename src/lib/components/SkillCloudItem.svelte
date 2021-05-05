aab650235bc97a001780cf29254d0724bd2ffb2f
Bryan Lee <bryanmylee@gmail.com>
2021-05-06 01:59:49 +0800

    feat: add an arrow for the tooltip

--- src/lib/components/SkillCloudItem.svelte
+++ src/lib/components/SkillCloudItem.svelte
@@ -6,16 +6,19 @@
   export let description = '';
   export let imageSrc = '';
 
-  const [ref, content] = createPopperActions();
+  const [ref, tooltip] = createPopperActions({
+    modifiers: [{ name: 'offset', options: { offset: [0, 50] } }]
+  });
   let showPopper = false;
 </script>
 
 {#if showPopper}
-  <div use:content class="fixed z-10 p-4 space-y-2 w-80 bg-base-lighter">
+  <div use:tooltip class="fixed z-10 p-4 space-y-2 w-80 bg-base-lighter">
     <h2 class="font-mono text-xl font-bold text-yellow">{name}</h2>
     <p class="text-sm leading-6">
       {description}
     </p>
+    <div data-popper-arrow class="absolute border border-white w-0 h-[50px]" />
   </div>
 {/if}
 
@@ -29,3 +32,13 @@
     class="z-0 w-28 h-28 min-w-28 min-h-28"
   />
 </CursorGravity>
+
+<style lang="postcss">
+  :global([data-popper-placement^='top'] > [data-popper-arrow]) {
+    bottom: -50px;
+  }
+
+  :global([data-popper-placement^='bottom'] > [data-popper-arrow]) {
+    top: -58px;
+  }
+</style>
