fcce81e9111e93e98c6f1bf064ae9dfb24e47bd7
Bryan Lee <bryanmylee@gmail.com>
2021-05-05 17:36:46 +0800

    feat: data on skills

---
+++ src/lib/db/skills.ts
@@ -0,0 +1,86 @@
+import type { Skill } from '$lib/models/Skill';
+
+export const skills: Skill[] = [
+  {
+    name: 'TypeScript',
+    description:
+      'Probably my favourite and most proficient language. I love the powerful type system, and always use TypeScript for web development as much as possible.',
+    imageSrc: 'skills/typescript.svg'
+  },
+  {
+    name: 'JavaScript',
+    description:
+      "It's a reliable and necessary language for the web. While I prefer to use TypeScript, some projects are simpler with JavaScript.",
+    imageSrc: 'skills/javascript.svg'
+  },
+  {
+    name: 'Svelte',
+    description:
+      "My favourite frontend framework for the web. I've built many projects on Svelte and have also contributed to the ecosystem and tooling.",
+    imageSrc: 'skills/svelte.svg'
+  },
+  {
+    name: 'React',
+    description:
+      'The industry standard for frontend web development. I started my web development journey with React and I still use it occasionally.',
+    imageSrc: 'skills/reactjs.svg'
+  },
+  {
+    name: 'Vim',
+    description:
+      "I'm obsessed with Vim and its philosophy of modal editing! Most of my development work is done in NeoVim with coc.nvim plugins. If I do have to use other editors, I usually reach for a comparable extension.",
+    imageSrc: 'skills/vim.svg'
+  },
+  {
+    name: 'C',
+    description:
+      'I have a love hate relationship with C. The language is simple and powerful, and sometimes I enjoy working close to memory. However, the standard library and tooling could be improved.',
+    imageSrc: 'skills/c.svg'
+  },
+  {
+    name: 'Python',
+    description:
+      "My go-to language for algorithm designa and scripting. I'm extremely proficient in Python.",
+    imageSrc: 'skills/python.svg'
+  },
+  {
+    name: 'Node.js',
+    description: 'I am comfortable with Node.js and Express. I use Node.js for quick projects.',
+    imageSrc: 'skills/nodejs.svg'
+  },
+  {
+    name: 'Firebase',
+    description: "I use Firebase for any quick projects where a relational database isn't needed.",
+    imageSrc: 'skills/firebase.svg'
+  },
+  {
+    name: 'MySQL',
+    description:
+      'I am proficient in database design. I usually use an ORM with MySQL, but I can also write optimized queries and take advantage of indices.',
+    imageSrc: 'skills/mysql.svg'
+  },
+  {
+    name: 'Tailwind CSS',
+    description:
+      'My favourite CSS framework. I use Tailwind CSS extensively and have contributed custom plugins to the community.',
+    imageSrc: 'skills/tailwindcss.svg'
+  },
+  {
+    name: 'Swift',
+    description:
+      'I started developing personal projects on iOS with Swift and the UIKit framework in 2017. The language has remained one of my favourites since, and I look forward to learning SwiftUI.',
+    imageSrc: 'skills/swift.svg'
+  },
+  {
+    name: 'Java',
+    description:
+      'I am proficient in Java. I am also an Oracle Certified Professional for Java SE 11.',
+    imageSrc: 'skills/java.svg'
+  },
+  {
+    name: 'Spring',
+    description:
+      'One of my favourite software design frameworks. I use Spring Boot extensively and have applied the dependency injection principle to other projects.',
+    imageSrc: 'skills/springio.svg'
+  }
+];
