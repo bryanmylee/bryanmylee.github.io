c1fd739a4f47bd8d53525009f63b0540edb7cc33
Bryan Lee <bryanmylee@gmail.com>
2021-05-06 04:48:25 +0800

    build: upgrade sveltekit static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,6 +12,7 @@ const config = {
   ],
 
   kit: {
+    adapter: adapter(),
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte'
   }
