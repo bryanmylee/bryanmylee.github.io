c0162f32d7f793013bbaf06a2cd8f8ee46483762
Bryan Lee <bryanmylee@gmail.com>
2021-05-05 14:09:37 +0800

    style: format

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,9 +1,9 @@
 <div class="relative font-mono">
   <div class="absolute inset-0 bg-yellow -z-1 nav-bg" style={`--bg: url(nav-bg.svg)`} />
   <div class="flex items-stretch justify-between w-full max-w-screen-md mx-auto">
-    <a href="/" class="flex items-center p-2 font-mono text-base text-xl font-bold bg-yellow"
-      >Bryan Lee</a
-    >
+    <a href="/" class="flex items-center p-2 font-mono text-base text-xl font-bold bg-yellow">
+      Bryan Lee
+    </a>
   </div>
 </div>
 
