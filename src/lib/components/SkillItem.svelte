dd3862c1276b5dfd6e275a2acc15e4045ec9ec88
Bryan Lee <bryanmylee@gmail.com>
2021-05-06 16:42:25 +0800

    feat: fly skills in

--- src/lib/components/SkillItem.svelte
+++ src/lib/components/SkillItem.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import { fly } from 'svelte/transition';
+
   export let name = '';
   export let description = '';
   export let imageSrc = '';
 </script>
 
-<li class="p-6 space-y-4 bg-dark-gray-lighter">
+<li class="p-6 space-y-4 bg-dark-gray-lighter" in:fly={{ y: 100 }}>
   <div class="flex items-center space-x-4">
     <img src={imageSrc} alt={name} class="w-16 h-16" />
     <h2 class="font-mono text-xl font-bold">
