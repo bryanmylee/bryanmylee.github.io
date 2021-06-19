0977ba565703022a85fd09c64ea4a3d60bb2a9df
Bryan Lee <bryanmylee@gmail.com>
2021-06-20 01:32:36 +0800

    feat: pass class through mount at intersection

--- src/lib/components/MountAtIntersection.svelte
+++ src/lib/components/MountAtIntersection.svelte
@@ -3,9 +3,12 @@
 
   let mountAtPercent: number;
   export { mountAtPercent as percent };
+
+  let className = '';
+  export { className as class };
 </script>
 
-<IntersectionObserver let:percent unobserveAt={mountAtPercent}>
+<IntersectionObserver let:percent unobserveAt={mountAtPercent} class={className}>
   {#if percent > mountAtPercent}
     <slot />
   {/if}
