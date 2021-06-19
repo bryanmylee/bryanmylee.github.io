26aa3afdd0a21d0d828a831f31b63da5ca47b068
Bryan Lee <bryanmylee@gmail.com>
2021-06-19 21:26:38 +0800

    fix: remove __layout due to breaking

--- src/routes/__layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import '../app.postcss';
-  import Nav from '$lib/components/Nav.svelte';
-</script>
-
-<Nav />
-<main class="z-0 max-w-screen-md mx-auto">
-  <slot />
-</main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import '../app.postcss';
   import { CircleIcon, ListIcon } from 'svelte-feather-icons';
   import CompetitionItem from '$lib/components/CompetitionItem.svelte';
   import ContactCard from '$lib/components/ContactCard.svelte';
   import ExperienceItem from '$lib/components/ExperienceItem.svelte';
   import GlitchMarquee from '$lib/components/GlitchMarquee.svelte';
   import Hero from '$lib/components/Hero.svelte';
+  import Nav from '$lib/components/Nav.svelte';
   import ProjectItem from '$lib/components/ProjectItem.svelte';
   import SkillCloud from '$lib/components/SkillCloud.svelte';
   import SkillItem from '$lib/components/SkillItem.svelte';
@@ -29,81 +31,84 @@
   <title>Bryan Lee</title>
 </svelte:head>
 
-<p class="mx-4 md:mx-0 mt-12 font-mono text-3xl">
-  I am a <GlitchMarquee
-    interval={5000}
-    texts={descriptions}
-    class="font-bold text-cyan inline-block"
-  />
-</p>
+<Nav />
+<main class="z-0 max-w-screen-md mx-auto">
+  <p class="mx-4 md:mx-0 mt-12 font-mono text-3xl">
+    I am a <GlitchMarquee
+      interval={5000}
+      texts={descriptions}
+      class="font-bold text-cyan inline-block"
+    />
+  </p>
 
-<Hero class="mt-12" />
+  <Hero class="mt-12" />
 
-<h1 id="projects">Projects</h1>
+  <h1 id="projects">Projects</h1>
 
-<ul class="grid mx-4 md:mx-0 md:grid-cols-2 gap-8">
-  {#each projects as project}
-    <ProjectItem {...project} />
-  {/each}
-</ul>
+  <ul class="grid mx-4 md:mx-0 md:grid-cols-2 gap-8">
+    {#each projects as project}
+      <ProjectItem {...project} />
+    {/each}
+  </ul>
 
-<h1 id="competitions">Competitions</h1>
+  <h1 id="competitions">Competitions</h1>
 
-<ul class="block space-y-4">
-  {#each competitions as competition}
-    <CompetitionItem {...competition} />
-  {/each}
-</ul>
+  <ul class="block space-y-4">
+    {#each competitions as competition}
+      <CompetitionItem {...competition} />
+    {/each}
+  </ul>
 
-<h1 id="skills">
-  <div class="flex items-center justify-center md:justify-between">
-    <div class="w-8 hidden md:block" />
-    <div>Skills</div>
-    <button
-      on:click={() => (showSkillCloud = !showSkillCloud)}
-      class="w-8 hidden md:block text-white"
-    >
-      {#if showSkillCloud}
-        <ListIcon />
-      {:else}
-        <CircleIcon />
-      {/if}
-    </button>
-  </div>
-</h1>
+  <h1 id="skills">
+    <div class="flex items-center justify-center md:justify-between">
+      <div class="w-8 hidden md:block" />
+      <div>Skills</div>
+      <button
+        on:click={() => (showSkillCloud = !showSkillCloud)}
+        class="w-8 hidden md:block text-white"
+      >
+        {#if showSkillCloud}
+          <ListIcon />
+        {:else}
+          <CircleIcon />
+        {/if}
+      </button>
+    </div>
+  </h1>
 
-<section class="flex flex-col items-end min-h-[400px]">
-  {#if $media.md && showSkillCloud}
-    <SkillCloud {skills} />
-  {:else}
-    <ul class="grid sm:grid-cols-2 sm:px-4 md:px-0 gap-4">
-      {#each skills as skill, index}
-        <SkillItem {...skill} {index} />
-      {/each}
-    </ul>
-  {/if}
-</section>
+  <section class="flex flex-col items-end min-h-[400px]">
+    {#if $media.md && showSkillCloud}
+      <SkillCloud {skills} />
+    {:else}
+      <ul class="grid sm:grid-cols-2 sm:px-4 md:px-0 gap-4">
+        {#each skills as skill, index}
+          <SkillItem {...skill} {index} />
+        {/each}
+      </ul>
+    {/if}
+  </section>
 
-<h1 id="experience">Experience</h1>
+  <h1 id="experience">Experience</h1>
 
-<ul class="block space-y-4">
-  {#each experiences as experience}
-    <ExperienceItem {...experience} />
-  {/each}
-</ul>
+  <ul class="block space-y-4">
+    {#each experiences as experience}
+      <ExperienceItem {...experience} />
+    {/each}
+  </ul>
 
-<h1 id="contact">Contact</h1>
-<section
-  class="flex flex-col sm:flex-row items-stretch justify-stretch space-y-4 sm:space-y-0 sm:space-x-4"
->
-  <ContactCard title="github" subtitle="bryanmylee" href="https://github.com/bryanmylee" />
-  <ContactCard title="email" subtitle="bryanmylee@gmail.com" href="mailto:bryanmylee@gmail.com" />
-  <ContactCard
-    title="linkedin"
-    subtitle="bryan-lee-min-yuan"
-    href="https://www.linkedin.com/in/bryan-lee-min-yuan/"
-  />
-</section>
+  <h1 id="contact">Contact</h1>
+  <section
+    class="flex flex-col sm:flex-row items-stretch justify-stretch space-y-4 sm:space-y-0 sm:space-x-4"
+  >
+    <ContactCard title="github" subtitle="bryanmylee" href="https://github.com/bryanmylee" />
+    <ContactCard title="email" subtitle="bryanmylee@gmail.com" href="mailto:bryanmylee@gmail.com" />
+    <ContactCard
+      title="linkedin"
+      subtitle="bryan-lee-min-yuan"
+      href="https://www.linkedin.com/in/bryan-lee-min-yuan/"
+    />
+  </section>
+</main>
 
 <style lang="postcss">
   h1 {
