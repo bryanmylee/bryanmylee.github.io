ea38dc4d0848f1964484fe5a4da4247fe428b03d
Bryan Lee <bryanmylee@gmail.com>
2021-06-22 21:49:11 +0800

    feat: rewording

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -6,7 +6,7 @@
 <section class="{className} bg-dark-gray-lighter relative p-8 space-y-8">
   <h1 class="font-mono text-4xl font-bold text-yellow">Bryan Lee (bryanmylee)</h1>
   <p>I am a computer geek with a passion for software development.</p>
-  <p>I build tools and scripts that solve real-world problems.</p>
+  <p>I build solutions and tools that solve real-world problems.</p>
   <a href="#projects" class="block font-mono text-2xl font-bold text-cyan hover:underline"
     >Check them out here.</a
   >
