23fff47b71a804dfd7a65482c117ea7037d90f5e
Bryan Lee <bryanmylee@gmail.com>
2021-11-03 04:39:20 +0800

    fix: deploy script removing custom domain

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,15 +3,18 @@ import replace from 'replace-in-file';
 import ghpages from 'gh-pages';
 
 replace({
-  files: 'build/**/*',
-  from: /_app/g,
-  to: 'app'
+	files: 'build/**/*',
+	from: /_app/g,
+	to: 'app',
 })
-  .then(() => {
-    return fs.rename('build/_app', 'build/app');
-  })
-  .then(() => {
-    ghpages.publish('build', () => {
-      console.log('Deploy complete!');
-    });
-  });
+	.then(() => {
+		return fs.rename('build/_app', 'build/app');
+	})
+	.then(() => {
+		return fs.writeFile('build/CNAME', 'bryanmylee.com');
+	})
+	.then(() => {
+		ghpages.publish('build', () => {
+			console.log('Deploy complete!');
+		});
+	});
