2a3ecaa8008212bd7ec2cc568a4f8b37555bcb4c
Bryan Lee <bryanmylee@gmail.com>
2021-12-19 16:48:39 +0800

    feat: svelte-popperjs example

---
+++ src/lib/components/util/Toggle.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	let showAlt = false;
+
+	let className = '';
+	export { className as class };
+</script>
+
+<div class="relative {className}">
+	<button
+		on:click={() => (showAlt = !showAlt)}
+		class:showAlt
+		class="absolute z-10 p-2 font-mono text-sm rounded-lg top-8 right-8">&lt;&#47;&gt;</button
+	>
+	{#if showAlt}
+		<slot name="alt" />
+	{:else}
+		<slot />
+	{/if}
+</div>
+
+<style lang="postcss">
+	.showAlt {
+		@apply bg-cyan;
+	}
+</style>
--- src/routes/_projects.svelte
+++ src/routes/_projects.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { projects } from '$lib/db/projects';
+	import SveltePopperJs from '../lib/components/project/SveltePopperJs.svelte';
 	import ProjectItem from '../lib/components/project/ProjectItem.svelte';
 </script>
 
 <h1 id="projects" class="header">Projects</h1>
+
+<SveltePopperJs />
 
 <ul class="grid max-w-screen-md gap-8 pr-4 mx-auto lg:pr-0 lg:max-w-screen-lg lg:grid-cols-2">
 	{#each projects as project}
