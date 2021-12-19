1be4b273daae4828b3a8272df8505be66fb9b13b
Bryan Lee <bryanmylee@gmail.com>
2021-12-19 23:35:01 +0800

    feat: show skill name on hover

--- src/lib/components/skill/SkillChip.svelte
+++ src/lib/components/skill/SkillChip.svelte
@@ -6,7 +6,10 @@
 </script>
 
 {#if skillItem}
-	<li>
+	<li class="relative group">
 		<img src={skillItem.imageSrc} alt={skillItem.name} class="w-6 h-6" />
+		<div class="absolute hidden p-2 mt-2 text-sm border border-white top-full group-hover:block">
+			{skillItem.name}
+		</div>
 	</li>
 {/if}
