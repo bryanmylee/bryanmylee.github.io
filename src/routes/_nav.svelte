dc886fb19ad5ea02d67cb9b7760d68fe870400e3
Bryan Lee <bryanmylee@gmail.com>
2021-12-21 02:32:57 +0800

    perf: reduce js dependency

--- src/routes/_nav.svelte
+++ src/routes/_nav.svelte
@@ -8,26 +8,24 @@
 	$: $media.md, (isOpen = false);
 </script>
 
-<nav class="sticky top-0 z-10 bg-opacity-60 bg-dark-gray">
+<nav class="sticky top-0 z-10 bg-shade">
 	<div class="relative">
 		<div class="absolute inset-0 bg-yellow z-[-1] nav-bg" />
 		<div class="flex items-stretch justify-between w-full max-w-screen-md mx-auto">
-			<a href="/#home" class="flex items-center p-4 text-xl nav-item"> Bryan Lee </a>
+			<a href="/#" class="flex items-center p-4 text-xl nav-item"> Bryan Lee </a>
 			<div class="relative">
-				{#if !$media.md}
-					<button
-						on:click={() => (isOpen = !isOpen)}
-						class="h-full border-transparent rounded-none nav-item focus:outline-none"
-					>
-						<MenuIcon class="w-6 h-6" />
-					</button>
-				{/if}
+				<button
+					on:click={() => (isOpen = !isOpen)}
+					class="h-full border-transparent rounded-none nav-item focus:outline-none md:hidden"
+				>
+					<MenuIcon class="w-6 h-6" />
+				</button>
 				{#if $media.md || isOpen}
 					<ul
 						transition:fade|local={{ duration: $media.md ? 0 : 200 }}
 						on:click={() => (isOpen = false)}
 						use:clickOutside={() => setTimeout(() => (isOpen = false))}
-						class="absolute right-0 flex flex-col h-full max-w-screen-md gap-4 md:static md:flex-row"
+						class="absolute right-0 flex flex-col h-full max-w-screen-md gap-2 md:gap-4 md:static md:flex-row"
 					>
 						<li class="contents"><a href="/#projects" class="nav-item">projects</a></li>
 						<li class="contents"><a href="/#experience" class="nav-item">experience</a></li>
