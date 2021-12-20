3e474349aca57e834953846d4ac0632457060c1d
Bryan Lee <bryanmylee@gmail.com>
2021-12-20 22:49:04 +0800

    style: nowrap on skill chip whitespace

--- src/lib/components/skill/SkillChip.svelte
+++ src/lib/components/skill/SkillChip.svelte
@@ -9,7 +9,7 @@
 	<li class="relative group">
 		<img src={skillItem.imageSrc} alt={skillItem.name} class="w-6 h-6" />
 		<div
-			class="absolute hidden p-2 mb-2 text-sm border border-white bottom-full group-hover:block bg-shade"
+			class="absolute p-2 mb-2 text-sm border border-white opacity-0 pointer-events-none bottom-full group-hover:opacity-100 bg-shade whitespace-nowrap"
 		>
 			{skillItem.name}
 		</div>
