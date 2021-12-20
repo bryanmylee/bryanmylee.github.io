8a72147b69a7262f8f1ecfa30c0c1eff03e7b00e
Bryan Lee <bryanmylee@gmail.com>
2021-12-21 04:18:55 +0800

    style: rewording

--- src/lib/db/projects.ts
+++ src/lib/db/projects.ts
@@ -51,7 +51,7 @@ export const projects: Record<ProjectID, Project> = {
 	helloworld: {
 		name: 'HelloWorld',
 		description:
-			'An AR application to relive and reminiscence your travel adventures in the face of border restrictions.',
+			'An AR application to re-live and reminiscence your travel adventures in the face of border restrictions.',
 		imageSrc: 'projects/helloworld.png',
 		href: 'https://news.smu.edu.sg/news/2021/07/02/smus-computing-and-information-systems-team-emerged-champions-dsta-hackathon',
 	},
