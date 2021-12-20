afec0fe8e0a3cf88124c025791b67ca8382615fd
Bryan Lee <bryanmylee@gmail.com>
2021-12-20 21:16:38 +0800

    style: allow laptop to overflow

--- src/lib/components/project/Meetwhen.svelte
+++ src/lib/components/project/Meetwhen.svelte
@@ -3,31 +3,47 @@
 	import ProjectItem from './ProjectItem.svelte';
 </script>
 
-<section class="max-w-screen-md gap-4 p-4 mx-auto mb-8">
-	<div class="z-10">
-		<ProjectItem projectId="meetwhen.io" />
-	</div>
-	<div class="relative h-[400px] z-0">
-		<RevealOnIntersection percent={100} transition="fly" flyY={50} duration={600} delay={250}>
-			<img
-				src="/projects/meetwhen/desktop-left-light.png"
-				alt="meetwhen.io desktop preview"
-				class="object-cover h-[400px] absolute inset-0 left-auto"
-			/>
-		</RevealOnIntersection>
-		<RevealOnIntersection percent={100} transition="fly" flyY={50} duration={600} delay={50}>
-			<img
-				src="/projects/meetwhen/mobile-right-dark.png"
-				alt="meetwhen.io desktop preview"
-				class="object-cover h-[400px] absolute inset-0 right-auto"
-			/>
-		</RevealOnIntersection>
-	</div>
-	<RevealOnIntersection percent={50} transition="fly" flyY={50} duration={600} delay={450}>
-		<div class="flex items-center justify-center mt-4">
-			<a href="https://meetwhen.io" class="p-4 transition bg-shade hover:text-cyan">
-				Find the best time for everyone.
-			</a>
+<div class="w-full overflow-x-clip">
+	<section class="max-w-screen-md gap-4 p-4 mx-auto mb-8">
+		<div class="z-10">
+			<ProjectItem projectId="meetwhen.io" />
 		</div>
-	</RevealOnIntersection>
-</section>
+		<div class="relative h-[400px] z-0">
+			<RevealOnIntersection
+				percent={100}
+				transition="fly"
+				flyY={50}
+				duration={600}
+				delay={250}
+				class="absolute inset-0 left-auto -mx-4"
+			>
+				<img
+					src="/projects/meetwhen/desktop-left-light.png"
+					alt="meetwhen.io desktop preview"
+					class="object-cover w-full h-full"
+				/>
+			</RevealOnIntersection>
+			<RevealOnIntersection
+				percent={100}
+				transition="fly"
+				flyY={50}
+				duration={600}
+				delay={50}
+				class="absolute inset-0 right-auto"
+			>
+				<img
+					src="/projects/meetwhen/mobile-right-dark.png"
+					alt="meetwhen.io desktop preview"
+					class="object-cover w-full h-full"
+				/>
+			</RevealOnIntersection>
+		</div>
+		<RevealOnIntersection percent={50} transition="fly" flyY={50} duration={600} delay={450}>
+			<div class="flex items-center justify-center mt-4">
+				<a href="https://meetwhen.io" class="p-4 transition bg-shade hover:text-cyan">
+					Find the best time for everyone.
+				</a>
+			</div>
+		</RevealOnIntersection>
+	</section>
+</div>
