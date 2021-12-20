06f6ef048e8426520db9d9e9efd4d521ba962770
Bryan Lee <bryanmylee@gmail.com>
2021-12-21 00:57:32 +0800

    feat: add action to extrude button

--- src/lib/components/project/TailwindCssExtrude.svelte
+++ src/lib/components/project/TailwindCssExtrude.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
+	import { fly, scale } from 'svelte/transition';
 	import { HighlightSvelte } from 'svelte-highlight';
 	import github from 'svelte-highlight/src/styles/github-dark';
 
 	import ProjectItem from './ProjectItem.svelte';
 	import Toggle from '$lib/components/util/Toggle.svelte';
+
+	let showPop = false;
+	let timeout: NodeJS.Timeout | null = null;
+	const handleClick = () => {
+		showPop = true;
+		if (timeout) {
+			clearTimeout(timeout);
+		}
+		timeout = setTimeout(() => {
+			showPop = false;
+		}, 400);
+	};
 
 	const exampleCode = `<button class="...extrude-cyan-darker
 	extrude-y-4 hover:extrude-y-2 active:extrude-y"
@@ -24,9 +37,19 @@
 	<Toggle class="flex-1 w-full md:w-1/2">
 		<div class="flex items-center justify-center example-area">
 			<button
-				class="px-4 py-2 font-bold transition border-4 rounded-full border-cyan-dark bg-cyan active:opacity-50 extrude-y-4 hover:extrude-y-2 active:extrude-y extrude-cyan-dark text-shade-dark"
+				on:click={handleClick}
+				class="relative px-4 py-2 font-bold transition border-4 rounded-full border-cyan-dark bg-cyan active:opacity-50 extrude-y-4 hover:extrude-y-2 active:extrude-y extrude-cyan-dark text-shade-dark"
 			>
 				Press me
+				{#if showPop}
+					<div
+						in:scale={{ duration: 80 }}
+						out:fly={{ y: -50 }}
+						class="absolute left-0 right-0 p-2 mb-4 border-4 rounded bottom-full bg-cyan border-cyan-dark"
+					>
+						pop!
+					</div>
+				{/if}
 			</button>
 		</div>
 		<p class="example-hint">Hover to try it out!</p>
