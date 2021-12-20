4e2d5b6a1e863e3a30f1bf32b00498ddf3cd614b
Bryan Lee <bryanmylee@gmail.com>
2021-12-21 01:42:42 +0800

    feat: whitehat experience

--- src/routes/_experiences.svelte
+++ src/routes/_experiences.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import Dso from '$lib/components/experience/Dso.svelte';
+	import Whitehat from '$lib/components/experience/Whitehat.svelte';
 	import Garuda from '$lib/components/experience/Garuda.svelte';
 </script>
 
 <h1 id="experience" class="header">Work Experience</h1>
 
 <Dso />
+<Whitehat />
 <Garuda />
