5f491937ac96e5d3638db39cb10de06fc4c528f0
Bryan Lee <bryanmylee@gmail.com>
2021-12-21 02:13:23 +0800

    feat: skills section

---
+++ src/routes/_skills.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { skills } from '$lib/db/skills';
+	import type { Skill } from '$lib/models/Skill';
+
+	const skillItems = Object.values(skills) as Skill[];
+</script>
+
+<h1 id="skills" class="header">Skills</h1>
+
+<section class="max-w-screen-md p-4 mx-auto">
+	<ul class="flex flex-wrap justify-between gap-4 text-sm">
+		{#each skillItems as { imageSrc, name }}
+			<li class="flex items-center justify-center gap-4 p-2 grow last:grow-0 bg-shade">
+				<img src={imageSrc} alt={name} class="w-8 h-8" />
+				<p class="whitespace-nowrap">
+					{name}
+				</p>
+			</li>
+		{/each}
+	</ul>
+</section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,12 @@
 <script>
 	import '../app.css';
+	import Nav from './_nav.svelte';
+	import Header from './_header.svelte';
 	import Contact from './_contact.svelte';
+	import Projects from './_projects.svelte';
+	import Skills from './_skills.svelte';
 	import Experiences from './_experiences.svelte';
 	import Footer from './_footer.svelte';
-	import Header from './_header.svelte';
-	import Nav from './_nav.svelte';
-	import Projects from './_projects.svelte';
 </script>
 
 <svelte:head>
@@ -17,6 +18,7 @@
 	<Header />
 	<Contact />
 	<Projects />
+	<Skills />
 	<Experiences />
 	<Footer />
 </main>
